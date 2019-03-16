b3986083f20a066c24b55cd6e1cedeb4034d1cce
Kiri Kuroda <kuroda.kiri@gmail.com>
2019-03-17 00:47:36 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # gazedata
-This is a beta version. No warranty.
+
+This is a Python module to handle gaze data (eyetracking data).
+
+**DISCLAIMER: This is a beta version. No warranty.**
+
+
+
+## License
+
+GPLv3 (<https://github.com/kirikuroda/gazedata/blob/master/LICENSE>)
+
+
+
+## Author
+
+Kiri Kuroda (<https://kirikuroda.github.io>)
+
+
+
+## Sample codes
+
--- setup.py
+++ setup.py
@@ -2,3 +2,30 @@
 from setuptools import setup
 import sys
 import os
+
+version = '0.0.1'
+
+setup(name = 'gazedata',
+      version = version,
+      description = 'Module for gaze-data handling',
+      long_description = """
+This is a Python module to handle gaze data (eyetracking gata).
+DISCLAIMER: This is a beta version. No warranty.
+""",
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering',
+      ],
+      keywords = 'Tobii, Eye tracking, eyetracking, gaze',
+      author = 'Kiri Kuroda',
+      url = 'https://github.com/kirikuroda/gazedata/',
+      licence = 'GNU GPL',
+      packages = ['gazedata'],
+      )
